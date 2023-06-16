a88c04dc46243b820f17c8281d5ed4f04fb29d8d
virali-b-patel <virali678@gmail.com>
2023-06-16 14:48:12 +0530

    Today-Plan

---
+++ get-github-users-data/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import UseEffectAPI from "./components/useEffect/useEffectAPI";
+
+function App() {
+  return (
+    <div>
+      <UseEffectAPI />
+    </div>
+  );
+}
+
+export default App;
